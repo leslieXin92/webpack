759bd678223b3e4e195d0665d03fd3d85f464b5a
Lixin Zhang <wb.zhanglixin01@mesg.corp.netease.com>
2024-04-05 23:27:13 +0800

    feat: webpack处理图片资源

---
+++ 02_配置文件(其他资源)/src/main.js
@@ -0,0 +1,10 @@
+import { sum, mul } from './js/math.js'
+import './js/component.js'
+
+const { now, dateFormat } = require('./js/date')
+
+console.log('sum', sum(1, 2))
+console.log('mul', mul(2, 3))
+
+console.log('now', now())
+console.log('formattedNow', dateFormat(new Date(), 'YYYY-MM-DD'))
---
+++ 02_配置文件(其他资源)/webpack.custom.config.js
@@ -0,0 +1,52 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build') // 必须是绝对路径
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // 正则匹配资源
+        use: [ // 从后往前执行
+          'style-loader',  // { loader: 'style-loader' } 简写形式
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1 // 为了处理文件import导致引入的文件未被调用下边的loader, 下边有几个loader就写几
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2
+            }
+          },
+          'postcss-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 34 * 1024 // 34 kB
+          }
+        },
+        generator: {
+          filename: 'img/[hash][ext][query]'
+        }
+      }
+    ]
+  }
+}
